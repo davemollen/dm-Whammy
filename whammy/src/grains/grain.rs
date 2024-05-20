f23c916010c2aca099fa716cce323263d318e1d5
Dave Mollen <dave.mollen@infi.nl>
2024-05-20 13:24:11 +0200

    Use wrap function instead of module

--- whammy/src/grains/grain.rs
+++ whammy/src/grains/grain.rs
@@ -1,10 +1,12 @@
 mod ramp;
-use ramp::Ramp;
-use std::f32::consts::PI;
 use super::VOICES;
 use crate::shared::{
-  delay_line::{DelayLine, Interpolation}, delta::Delta, float_ext::FloatExt
+  delay_line::{DelayLine, Interpolation},
+  delta::Delta,
+  float_ext::FloatExt,
 };
+use ramp::Ramp;
+use std::f32::consts::PI;
 
 #[derive(Clone)]
 pub struct Grain {
@@ -12,7 +14,7 @@ pub struct Grain {
   window_size: f32,
   time_ramp: Ramp,
   phase_offset: f32,
-  delta: Delta
+  delta: Delta,
 }
 
 impl Grain {
@@ -22,22 +24,36 @@ impl Grain {
       window_size: 0.,
       time_ramp: Ramp::new(sample_rate),
       phase_offset: (VOICES as f32).recip() * index as f32,
-      delta: Delta::new()
+      delta: Delta::new(),
     }
   }
 
-  pub fn process(&mut self, grain_delay_line: &mut DelayLine, phasor: f32, freq: f32, speed: f32) -> f32 {
-    let phase = (phasor + self.phase_offset) % 1.;
+  pub fn process(
+    &mut self,
+    grain_delay_line: &mut DelayLine,
+    phasor: f32,
+    freq: f32,
+    speed: f32,
+  ) -> f32 {
+    let phase = Self::wrap(phasor + self.phase_offset);
     let trigger = self.delta.process(phase).abs() > 0.5;
     if trigger {
       self.freq = freq;
       self.window_size = 1000. / freq;
       self.time_ramp.start();
     }
-    
+
     let ramp = self.time_ramp.process(self.freq * speed);
     let time = ramp * self.window_size;
     let window = (ramp * PI).fast_sin() * (phase * PI).fast_sin();
     grain_delay_line.read(time, Interpolation::Spline) * window
+  }
+
+  fn wrap(input: f32) -> f32 {
+    if input > 1. {
+      input - 1.
+    } else {
+      input
+    }
   }
 }
