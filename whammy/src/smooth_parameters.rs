66854b8c6e684f55d2c82416b5345fb8c871680a
Dave Mollen <dave.mollen@infi.nl>
2024-05-23 15:59:01 +0200

    Update smooth_parameters.rs

--- whammy/src/smooth_parameters.rs
+++ whammy/src/smooth_parameters.rs
@@ -1,34 +1,31 @@
 use crate::shared::ramp_smooth::RampSmooth;
 
-const SPEED_RAMP_FREQ: f32 = 50.;
-const GAIN_RAMP_TIME: f32 = 20.;
-
 pub struct SmoothParameters {
-  filters: [RampSmooth; 3],
+  smooth_speed: RampSmooth,
+  smooth_dry_level: RampSmooth,
+  smooth_wet_level: RampSmooth,
 }
 
 impl SmoothParameters {
   pub fn new(sample_rate: f32) -> Self {
     Self {
-      filters: [
-        RampSmooth::new(sample_rate, SPEED_RAMP_FREQ),
-        RampSmooth::new(sample_rate, GAIN_RAMP_TIME),
-        RampSmooth::new(sample_rate, GAIN_RAMP_TIME),
-      ],
+      smooth_speed: RampSmooth::new(sample_rate, 50.),
+      smooth_dry_level: RampSmooth::new(sample_rate, 20.),
+      smooth_wet_level: RampSmooth::new(sample_rate, 20.),
     }
   }
 
   pub fn initialize(&mut self, (speed, dry_level, wet_level): (f32, f32, f32)) {
-    self.filters[0].initialize(speed);
-    self.filters[1].initialize(dry_level);
-    self.filters[2].initialize(wet_level);
+    self.smooth_speed.initialize(speed);
+    self.smooth_dry_level.initialize(dry_level);
+    self.smooth_wet_level.initialize(wet_level);
   }
 
   pub fn process(&mut self, speed: f32, dry_level: f32, wet_level: f32) -> (f32, f32, f32) {
     (
-      self.filters[0].process(speed),
-      self.filters[1].process(dry_level),
-      self.filters[2].process(wet_level),
+      self.smooth_speed.process(speed),
+      self.smooth_dry_level.process(dry_level),
+      self.smooth_wet_level.process(wet_level),
     )
   }
 }
