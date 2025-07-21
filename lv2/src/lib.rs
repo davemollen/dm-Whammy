57c9850969468c77b456ec3054e6e0abf20d7e59
davemollen <davemollen@gmail.com>
2025-07-21 20:18:50 +0200

    Allow in-place processing

--- lv2/src/lib.rs
+++ lv2/src/lib.rs
@@ -5,11 +5,11 @@ use whammy::{Params, Whammy};
 
 #[derive(PortCollection)]
 struct Ports {
-  dry: InputPort<Control>,
-  wet: InputPort<Control>,
-  pitch: InputPort<Control>,
-  input: InputPort<Audio>,
-  output: OutputPort<Audio>,
+  dry: InputPort<InPlaceControl>,
+  wet: InputPort<InPlaceControl>,
+  pitch: InputPort<InPlaceControl>,
+  input: InputPort<InPlaceAudio>,
+  output: OutputPort<InPlaceAudio>,
 }
 
 #[uri("https://github.com/davemollen/dm-Whammy")]
@@ -39,10 +39,13 @@ impl Plugin for DmWhammy {
   // Process a chunk of audio. The audio ports are dereferenced to slices, which the plugin
   // iterates over.
   fn run(&mut self, ports: &mut Ports, _features: &mut (), _sample_count: u32) {
-    self.params.set(*ports.pitch, *ports.dry, *ports.wet);
+    self
+      .params
+      .set(ports.pitch.get(), ports.dry.get(), ports.wet.get());
 
-    for (input, output) in ports.input.iter().zip(ports.output.iter_mut()) {
-      *output = self.whammy.process(*input, &mut self.params);
+    for (input, output) in ports.input.iter().zip(ports.output.iter()) {
+      let whammy_output = self.whammy.process(input.get(), &mut self.params);
+      output.set(whammy_output);
     }
   }
 }
